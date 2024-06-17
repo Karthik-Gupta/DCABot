735d9f361e63b6e4bd01bf0ef9c17e6869e4ce7a
KG <karthikts.gupta@gmail.com>
2024-06-17 21:27:39 +0530

    Config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,37 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
+const SEPOLIA_URL = "https://sepolia.infura.io/v3/c9470e784de8487eaa7e08de876031bb";
+const SEPOLIA_CHAIN_ID = 11155111;
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        runs: 200,
+        enabled: true,
+      },
+    },
+  },
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      chainId: SEPOLIA_CHAIN_ID,
+      accounts: [
+        ...(process.env.SEPOLIA_PRIVATE_KEY_1
+          ? [process.env.SEPOLIA_PRIVATE_KEY_1]
+          : []),
+        ...(process.env.SEPOLIA_PRIVATE_KEY_2
+          ? [process.env.SEPOLIA_PRIVATE_KEY_2]
+          : []),
+      ],
+    },
+  },
 };
 
 export default config;
